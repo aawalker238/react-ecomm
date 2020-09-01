d0094dac225256884dc12fc69952317b19abdc7f
Anthony Walker <aawalker238@gmail.com>
2020-09-01 11:45:56 -0700

    initial commit and scss setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+// components
+import Homepage from "./homepage.component";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
